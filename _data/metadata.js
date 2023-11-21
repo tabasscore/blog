a78037105a18c12751538315abfa4e04471ee438
Antonin <antonin.demeilliez@protonmail.com>
2023-11-21 22:55:46 +0100

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Atozi",
+	url: "https://example.com/",
+	language: "fr",
+	description: "Narrative design en analyses",
+	author: {
+		name: "Antonin Demeilliez",
+		email: "antonin.demeilliez@protonmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
